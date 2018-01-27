40484e8b378176df2c99bd8de81d9cd699e16151
vklimov <vklimov@livemaster.ru>
2018-01-27 16:22:22 +0300

    MOD: нарисовал координатную сетку
    Signed-off-by: vklimov <vklimov@livemaster.ru>

--- snake.js
+++ snake.js
@@ -1,38 +1,47 @@
 'use strict';
-const SIZE_OF_SQUARE = 10;
+const DEFAULT_CANVAS_WIDTH = 600;
+const DEFAULT_CANVAS_HEIGHT = 600;
+const DEFAULT_CANVAS_BGCOLOR = '#cecece';
+const DEFAULT_CANVAS_BORDER = '1px solid #000';
+const SIZE_OF_SQUARE = 30;
+const GRID_LINE_WIDTH = 2;
+const GRID_LINE_COLOR = 'rgba(255, 0, 0, 0.5)';
 
 class Snake {
 	constructor(props) {
 		this.defaultProps = {
-			width: '400px',
-			height: '400px',
-			bgColor: '#cecece',
-			border: '1px solid #000'
+			width: DEFAULT_CANVAS_WIDTH,
+			height: DEFAULT_CANVAS_HEIGHT,
+			bgColor: DEFAULT_CANVAS_BGCOLOR,
+			border: DEFAULT_CANVAS_BORDER,
 		};
 		this.props = Object.assign(this.defaultProps, props);
 		this.axisX = [];
 		this.axisY = [];
 		this.ctx;
+		this.snake = [];
 	}
 
 	prepareCanvas() {
-		let width = parseInt(this.props.width, 10);
-		let height = parseInt(this.props.height, 10);
-		let remainW = width % SIZE_OF_SQUARE;
-		let remainH = height % SIZE_OF_SQUARE;
+		let width = this.props.width;
+		let height = this.props.height;
+		let step = SIZE_OF_SQUARE - GRID_LINE_WIDTH;
+		let remainW = width % step;
+		let remainH = height % step;
 
 		if (remainW !== 0) {
 			width += SIZE_OF_SQUARE - remainW;
-			this.props.width = width + 'px';
+			this.props.width = width;
 		}
 		if (remainH !== 0) {
 			height += SIZE_OF_SQUARE - remainH;
-			this.props.height = height + 'px';
+			this.props.height = height;
 		}
 
 		const canvas = document.getElementById('canvas');
-		canvas.style.width = this.props.width;
-		canvas.style.height = this.props.height;
+		
+		canvas.width = this.props.width;
+		canvas.height = this.props.height;
 		canvas.style.backgroundColor = this.props.bgColor;
 		canvas.style.border = this.props.border;
 		this.ctx = canvas.getContext('2d');
@@ -46,45 +55,40 @@ class Snake {
 
 	drawGrid() {
 		const ctx = this.ctx;
-		const height = parseInt(this.props.height, 10);
-		const width = parseInt(this.props.width, 10);
-		const lineWidth = 1;
+		const height = this.props.height;
+		const width = this.props.width;
 		const lineStartCoordinate = 0;
-		ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+		ctx.fillStyle = GRID_LINE_COLOR;
 		for (let i = 0; i < this.axisX.length; i++) {
-			ctx.fillRect(this.axisX[i], lineStartCoordinate, lineWidth, height);
+			ctx.fillRect(this.axisX[i], lineStartCoordinate, GRID_LINE_WIDTH, height);
 		}
 		for (let j = 0; j < this.axisY.length; j++) {
-			ctx.fillRect(lineStartCoordinate, this.axisY[j], width, lineWidth);
+			ctx.fillRect(lineStartCoordinate, this.axisY[j], width, GRID_LINE_WIDTH);
 		}
 	}
 
 	createCoordinates() {
-		let lengthX = parseInt(this.props.width, 10);
-		let lengthY = parseInt(this.props.height, 10);
-		let step = SIZE_OF_SQUARE - 1;
-		for (let i = 1; i <= lengthX; i += step) {
+		let lengthX = this.props.width - 1;
+		let lengthY = this.props.height - 1;
+		let step = SIZE_OF_SQUARE - GRID_LINE_WIDTH;
+		for (let i = 0; i <= lengthX; i += step) {
 			this.axisX.push(i);
 		}
-		for (let j = 1; j <= lengthY; j += step) {
+		for (let j = 0; j <= lengthY; j += step) {
 			this.axisY.push(j);
 		}
-		console.log(this.axisX[ 21 ]);
-		console.log(this.axisY[21]);
 	}
 
 	clear() {
 		this.ctx.clearRect(0, 0, this.props.width, this.props.height);
 	}
+
+	randomInteger(min, max) {
+		let rand = min - 0.5 + Math.random() * (max - min + 1);
+		rand = Math.round(rand);
+		return rand;
+	}
 }
 
 let snake = new Snake();
 snake.init();
-
-// ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-// fillRect(x, y, width, height)
-// Рисование заполненного прямоугольника.
-// strokeRect(x, y, width, height)
-// Рисование прямоугольного контура.
-// clearRect(x, y, width, height)
-// Очистка  прямоугольной области, делая содержимое совершенно прозрачным.
